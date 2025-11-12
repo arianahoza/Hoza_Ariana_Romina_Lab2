2f5bb3a33f592b4747e80d51744892c955cf725c
Ariana Hoza <ariana.hoza@gmail.com>
2025-11-12 20:46:02 +0200

    Actualizare Lab5

--- Data/Hoza_Ariana_Romina_Lab2Context.cs
+++ Data/Hoza_Ariana_Romina_Lab2Context.cs
@@ -19,5 +19,7 @@ namespace Hoza_Ariana_Romina_Lab2.Data
         public DbSet<Hoza_Ariana_Romina_Lab2.Models.Author> Author { get; set; } = default!;
         public DbSet<Hoza_Ariana_Romina_Lab2.Models.Category> Category { get; set; } = default!;
         public DbSet<BookCategory> BookCategory { get; set; }
+        public DbSet<Hoza_Ariana_Romina_Lab2.Models.Member> Member { get; set; } = default!;
+        public DbSet<Hoza_Ariana_Romina_Lab2.Models.Borrowing> Borrowing { get; set; } = default!;
     }
 }
